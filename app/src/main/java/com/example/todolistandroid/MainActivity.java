044e3100a3f29ea230e953ac678b4802c1e825f5
Eddy DU BOIS <102170180+Eddyteur@users.noreply.github.com>
2023-06-01 12:15:08 +0200

    Avancement frontend

--- app/src/main/java/com/example/todolistandroid/MainActivity.java
+++ app/src/main/java/com/example/todolistandroid/MainActivity.java
@@ -13,9 +13,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+
 }
