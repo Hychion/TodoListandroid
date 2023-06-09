0e861de16bc0e0df87ea8f63bde3424d98209a98
HaronMan <h.kochbati59@gmail.com>
2023-06-09 15:14:30 +0200

    Debut evenement

--- app/src/main/java/com/example/todolistandroid/EditActivity.java
+++ app/src/main/java/com/example/todolistandroid/EditActivity.java
@@ -2,13 +2,18 @@ package com.example.todolistandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.android.material.tabs.TabLayout;
 
@@ -24,6 +29,35 @@ public class EditActivity extends AppCompatActivity {
 
         TabLayout tabLayout = findViewById(R.id.categories);
         tabLayout.setTabMode(TabLayout.MODE_SCROLLABLE);
+
+        //Creation d'un nouveau EditText "tache"
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"})
+        ConstraintLayout constraintLayout = findViewById(R.id.page);
+        EditText editText = findViewById(R.id.task);
+        editText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String text = s.toString();
+                if (!text.isEmpty()) {
+                    EditText newEditText = new EditText(EditActivity.this);
+                    ConstraintLayout.LayoutParams params = new ConstraintLayout.LayoutParams(
+                            ConstraintLayout.LayoutParams.MATCH_PARENT,
+                            ConstraintLayout.LayoutParams.WRAP_CONTENT
+                    );
+                    newEditText.setLayoutParams(params);
+                    newEditText.setHint("Nouvelle Tache...");
+                    constraintLayout.addView(newEditText, constraintLayout.indexOfChild(editText) + 1);
+                    editText.removeTextChangedListener(this); // Pour éviter une boucle infinie
+                }
+            }
+        });
     }
 
     @Override
