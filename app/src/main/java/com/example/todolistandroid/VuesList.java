abb7f1463b449d85272915d55d5f9a0c8718dddc
LIBERT Quentin <quentinlibert59@gmail.com>
2023-06-09 15:23:44 +0200

    finalisation listview

---
+++ app/src/main/java/com/example/todolistandroid/VuesList.java
@@ -0,0 +1,13 @@
+package com.example.todolistandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VuesList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vues_list);
+    }
+}
