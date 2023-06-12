2e8bd66af607530abb7dec88752a3951f3be9770
Eddy DU BOIS <102170180+Eddyteur@users.noreply.github.com>
2023-06-12 11:48:41 +0200

    avancement changement taille police

--- app/src/main/java/com/example/todolistandroid/Options.java
+++ app/src/main/java/com/example/todolistandroid/Options.java
@@ -6,9 +6,10 @@ import androidx.appcompat.app.AppCompatDelegate;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.TypedValue;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CompoundButton;
-import android.widget.RadioGroup;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -17,31 +18,18 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemSelectedListener;
 
 public class Options extends AppCompatActivity implements OnItemSelectedListener {
-    String[] police = {"Petite", "Moyenne", "Grande"};
+    int taillePolice = 15;
 
     @SuppressLint("UseSwitchCompatOrMaterialCode")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.option_layout);
-        Spinner spinner = (Spinner) findViewById(R.id.font_spinner);
-
-        spinner.setOnItemSelectedListener(this);
-
-        List<String> categories = new ArrayList<String>();
-        categories.add("Petite");
-        categories.add("Moyenne");
-        categories.add("Grande");
-
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, categories);
-
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner.setAdapter(dataAdapter);
 
         Button appliquer = findViewById(R.id.button);
 
@@ -53,16 +41,45 @@ public class Options extends AppCompatActivity implements OnItemSelectedListener
             }
         });
 
-        Switch ThemeSwitch = findViewById(R.id.LightDark);
+        Switch themeSwitch = findViewById(R.id.LightDark);
 
-        ThemeSwitch.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener() {
+        themeSwitch.setChecked(PreferenceUtil.isDarkModeEnabled(this));
+
+        themeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                PreferenceUtil.saveThemeMode(Options.this, isChecked);
                 if (isChecked) {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
                 } else {
                     AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 }
+            }
+        });
+
+        Spinner spinnerTaillePolice = findViewById(R.id.spinnerTaillePolice);
+
+        spinnerTaillePolice.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (id == 0) {
+                    taillePolice = 12;
+                    View rootView = getWindow().getDecorView().getRootView();
+                    appliquerTaillePolice(rootView, taillePolice);
+                } else if (id == 1) {
+                    taillePolice = 15;
+                    View rootView = getWindow().getDecorView().getRootView();
+                    appliquerTaillePolice(rootView, taillePolice);
+                } else {
+                    taillePolice = 18;
+                    View rootView = getWindow().getDecorView().getRootView();
+                    appliquerTaillePolice(rootView, taillePolice);
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
     }
@@ -78,5 +95,23 @@ public class Options extends AppCompatActivity implements OnItemSelectedListener
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
         //TODO
+    }
+
+    private void appliquerTaillePolice(View view, float taillePolice) {
+        if (view instanceof ViewGroup) {
+            ViewGroup viewGroup = (ViewGroup) view;
+            int count = viewGroup.getChildCount();
+            for (int i = 0; i < count; i++) {
+                View child = viewGroup.getChildAt(i);
+                appliquerTaillePolice(child, taillePolice);
+            }
+        } else if (view instanceof TextView) {
+            TextView textView = (TextView) view;
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, taillePolice);
+        }
+    }
+
+    private int getTaillePolice() {
+        return taillePolice;
     }
 }
